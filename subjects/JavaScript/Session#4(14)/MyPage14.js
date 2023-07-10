78dcf5d7714fe1f96fcb2a8aa87e517030846d78
Zaina202 <92650788+Zaina202@users.noreply.github.com>
2023-07-10 19:32:39 +0300

    Session#4 (S14)

---
+++ subjects/JavaScript/Session#4(14)/MyPage14.js
@@ -0,0 +1,19 @@
+ function logInfo() {
+    console.log('My Info');
+}
+
+function logWarning() {
+    console.warn('My Warning');
+}
+
+function logError() {
+    console.error('My Error');
+}
+
+function logTable() {
+    console.table([
+        { Name: 'Zeyad Abed Aljalil'},
+        { Name: 'Ahmad Hanani'},
+        { Name: 'Ahmad Eran'}
+    ]);
+}
